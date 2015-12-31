867dfc5a02d52899220c5bae0eb540c6e8f0009c
captainblue2013 <admin@lanhao.name>
2015-12-31 13:31:04 +0800

    移除超级登录

--- controllers/access.js
+++ controllers/access.js
@@ -11,6 +11,9 @@ let access = {};
 
 access.version = '1';
 
+/**
+ * 普通登录
+ */
 access.login = function (req, res) { 
     let account = req.body.account;
     let password = req.body.password;
@@ -39,35 +42,109 @@ access.login = function (req, res) {
     }
 };
 
-//TODO 
+/**
+ * 超级登录
+ */
+
 access.spLogin = function (req, res) {
-    let appId = req.query.appId ;
-    let account = req.query.account;
-    let sign = req.query.sign;
-    let t = Date.now();
-    let redirectUrl = decodeURIComponent(req.query.callback);
-    
-    if (appId && account  && sign) {
-        let appKey = appModel.all[appId].appKey;
-        if (sign === md5(account + appKey + parseInt(t/global.app.config.params['SP_LOGIN_SIGN_TIME_LIMIT']))) {
-            console.log('sign ok')
-            //success 签名通过意味着uc承认这个account的合法性
-            //todo
-        } else {
-            if(redirectUrl) {
-                res.render('error.html', {'msg': '签名不对你糊弄谁呢？'});
-            }else{
-                res.json(401,{},'签名不正确');
-            }
-        }
-    } else {
-        if(redirectUrl) {
-            res.render('error.html', {'msg': '大师兄，callback、account、password和appId被二师兄抓走了。  '});
-        }else{
-            res.json(401,{},'参数缺失，请参看文档');
-        }
-    }
+    res.json(200,{},'很遗憾，超级登录功能已经被移除');
+    //let appId = req.query.appId ;
+    //let account = req.query.account;
+    //let sign = req.query.sign;
+    //let t = Date.now();
+    //let redirectUrl = req.query.callback;
+    //
+    //if (appId && account  && sign) {
+    //    let appKey = appModel.all[appId].appKey;
+    //    if (true || sign === md5(account + appKey + parseInt(t/global.app.config.params['SP_LOGIN_SIGN_TIME_LIMIT']))) {
+    //        console.log('sign ok')
+    //        //success 签名通过意味着uc承认这个account的合法性
+    //        userModel.match('/accounts/fyuc/u',account, function (e, r) {
+    //           if(!e){
+    //                if(r.count===0){
+    //                    // sp register
+    //                    userModel.add(account,{
+    //                        'accounts':{
+    //                            'fyuc':[{
+    //                                'u':account,
+    //                                'p':'*'
+    //                            }]
+    //                        },
+    //                        'preferences':{
+    //                            'apps':[
+    //                                appId
+    //                            ]
+    //                        }
+    //                    }, function (e1, r1) {
+    //                        if(!e1){
+    //                            let accessToken = md5('uc3.0' + account + Date.now());
+    //                            let userInfo = {
+    //                                account,
+    //                                accessToken
+    //                            };
+    //                            redis.saveExpireObj('session:' + account, userInfo, 3600 * 24 * 30, function (_e, _r) {
+    //                                if(_e)console.log(_e);
+    //                            });
+    //                            console.log(!!redirectUrl);
+    //                            if(!!redirectUrl){
+    //                                redirectUrl = (redirectUrl.indexOf('?')>0)?redirectUrl+'&account='+account+'&accessToken='+accessToken:redirectUrl+'?account='+account+'&accessToken='+accessToken;
+    //                                res.redirect(redirectUrl);
+    //                            }else{
+    //                                res.json(200,userInfo);
+    //                            }
+    //                        }else{
+    //                            if(redirectUrl) {
+    //                                res.render('error.html', {'msg': '数据库出了点问题(2)'});
+    //                            }else{
+    //                                res.json(500,{},'数据库出了点问题 (2)');
+    //                            }
+    //                        }
+    //                    });
+    //                }else{
+    //                    // sp login
+    //                    let accessToken = md5('uc3.0' + account + Date.now());
+    //                    let userInfo = {
+    //                        account,
+    //                        accessToken
+    //                    };
+    //                    redis.saveExpireObj('session:' + account, userInfo, 3600 * 24 * 30, function (_e, _r) {
+    //                        if(_e)console.log(_e);
+    //                    });
+    //                    console.log(!!redirectUrl);
+    //                    if(!!redirectUrl){
+    //                        redirectUrl = (redirectUrl.indexOf('?')>0)?redirectUrl+'&account='+account+'&accessToken='+accessToken:redirectUrl+'?account='+account+'&accessToken='+accessToken;
+    //                        res.redirect(redirectUrl);
+    //                    }else{
+    //                        res.json(200,userInfo);
+    //                    }
+    //                }
+    //           }else{
+    //               if(redirectUrl) {
+    //                   res.render('error.html', {'msg': '数据库出了点问题'});
+    //               }else{
+    //                   res.json(500,{},'数据库出了点问题 ');
+    //               }
+    //           }
+    //        });
+    //    } else {
+    //        if(redirectUrl) {
+    //            res.render('error.html', {'msg': '签名不对你糊弄谁呢？'});
+    //        }else{
+    //            res.json(401,{},'签名不正确');
+    //        }
+    //    }
+    //} else {
+    //    if(redirectUrl) {
+    //        res.render('error.html', {'msg': '大师兄，callback、account、password和appId被二师兄抓走了。  '});
+    //    }else{
+    //        res.json(401,{},'参数缺失，请参看文档');
+    //    }
+    //}
 };
+
+/**
+ * 注册
+ */
 
 access.reg = function (req, res) {
     let appId = req.body.appId;
