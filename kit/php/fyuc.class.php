0d9c997f736dad9a48184f0a24770c44ee680878
captainblue2013 <admin@lanhao.name>
2016-01-09 21:08:19 +0800

    no message

--- kit/php/fyuc.class.php
+++ kit/php/fyuc.class.php
@@ -25,9 +25,15 @@ class FYUC
 
     /**
      * @param bool|true $redirect 是否直接跳转,false时将返回 login_url
+     * @return string url
      */
     public function login($callback,$redirect = true){
-        
+        $url = self::UC_HOST.'/appId='.$this->_appId.'&callback='.urlencode($callback);
+        if($redirect){
+            header('location:'.$url);
+        }else{
+            return $url;
+        }
     }
 
 }
@@ -36,18 +42,34 @@ class httpAgent{
 
     public static function GET($url,$headers = array(),$data = array()){
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, $url);
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+        $CURL_HEADERS = array('Content-Type: application/json');
+        if(is_array($headers)){
+            foreach($headers as $k=>$v){
+                $CURL_HEADERS[] = $k.':'.$v;
+            }
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $CURL_HEADERS);
+
         $output = curl_exec($ch);
         curl_close($ch);
+
         return $output;
     }
 
     public static function POST($url,$headers = array(),$data = array()){
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, $url);
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
@@ -58,13 +80,15 @@ class httpAgent{
             }
         }
         curl_setopt($ch, CURLOPT_HTTPHEADER, $CURL_HEADERS);
+
         $output = curl_exec($ch);
         curl_close($ch);
+
         return $output;
     }
 }
 
-$r = httpAgent::POST('http://127.0.0.1:9528/index/debug',array(
+$r = httpAgent::GET('http://127.0.0.1:9528/index/debug',array(
     'appid'=>1000,
     'appkey'=>'29322987bd616276e8d4da9754cb0903'
 ),array(
